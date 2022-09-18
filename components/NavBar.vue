982e6c02541d1faa496750c78729edfa5902324c
Ven <CareTiger@users.noreply.github.com>
2022-09-18 18:50:37 -0500

    updates

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,56 +1,86 @@
 <template>
-    <teleport to="body">
-    <Transition mode="in-out"
-    enter-active-class="animate__animated animate__fadeInRight zoomIn"
-    leave-active-class="animate__animated animate__fadeOutRight zoomOut">    
     <div>
         <div class="relative z-10" role="dialog">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
+            <div class="fixed inset-0 bg-transparent bg-opacity-75"></div>
             <div class="fixed inset-0 z-10 overflow-y-auto">
-
                 <!-- Actual modal -->
                 <Transition
-                enter-active-class="animate__animated animate__fadeInRight zoomIn"
-                leave-active-class="animate__animated animate__fadeOutRight zoomOut"> 
-                <div class="grid min-h-full w-full place-items-end">
-                    <div class="bg-weather-secondary text-white rounded-lg min-h-screen w-full">
-
+                    name="nav"
+                    appear
+                    @before-enter="beforeEnter"
+                    @before-leave="leave"
+                >
+                    <div class="grid min-h-full w-full place-items-end">
+                        <div
+                            class="bg-weather-secondary text-white rounded-l-lg min-h-screen w-3/4"
+                        >
                             <div class="flex justify-end m-2 md:m-4">
-                                <span class="bg-weather-primary p-1 rounded-full flex items-center hover:cursor-pointer" @click="closeNav">
-                                    <i class="material-icons-outlined text-4xl md:text-6xl">close</i>
+                                <span
+                                    class="bg-weather-primary p-1 rounded-full flex items-center hover:cursor-pointer"
+                                    @click="closeNav"
+                                >
+                                    <i
+                                        class="material-icons-outlined text-4xl md:text-6xl"
+                                        >close</i
+                                    >
                                 </span>
                             </div>
 
-                            <div class="grid justify-start items-center text-4xl content-height px-4">
+                            <div
+                                class="grid justify-start items-center text-4xl content-height px-4"
+                            >
                                 <ul class="flex flex-col space-y-5">
                                     <li>
-                                        <nuxt-link to="/" class="flex flex-row justify-center items-center">
-                                            <span><i class="fa-solid fa-house"></i></span>
+                                        <nuxt-link
+                                            to="/"
+                                            class="flex flex-row justify-center items-center"
+                                        >
+                                            <span
+                                                ><i
+                                                    class="fa-solid fa-house"
+                                                ></i
+                                            ></span>
                                             <span class="ml-4">Home</span>
-                                        </nuxt-link> 
+                                        </nuxt-link>
                                     </li>
                                     <li>About</li>
                                 </ul>
-
                             </div>
-                    </div>        
-                </div>
+                        </div>
+                    </div>
                 </Transition>
-
             </div>
         </div>
     </div>
-    </Transition>
-    </teleport>
 </template>
 
-
 <script setup>
-    // TODO the transition has to be fixed.
-import { useSiteStore } from '~~/store/site';
+// TODO the transition has to be fixed.
+import { useSiteStore } from "~~/store/site";
+import gsap from "gsap";
 const siteStore = useSiteStore();
+const showNav = ref(true);
 
 const closeNav = () => {
-    siteStore.toggleNavbar()    
+    siteStore.toggleNavbar();
+};
+
+function beforeEnter(el) {
+    gsap.from(el, {
+        duration: 1.5,
+        opacity: 1,
+        x: 400,
+        ease: "power2.easeInOut",
+    });
+}
+
+function leave(el) {
+    console.log("leave");
+    gsap.to(el, {
+        duration: 2,
+        opacity: 0,
+        x: 400,
+        ease: "power1.easeInOut",
+    });
 }
 </script>
